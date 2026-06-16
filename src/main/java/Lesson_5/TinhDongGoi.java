1c8af63c3b2352b7fc09b8d15ebe9af177784b32
Quynh Cao Thi Thanh <caothanhquynh07012003@gmail.com>
2026-06-16 10:34:40 +0700

    tính đóng gói

---
+++ src/main/java/Lesson_5/TinhDongGoi.java
@@ -0,0 +1,22 @@
+package Lesson_5;
+
+public class TinhDongGoi {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        TinhDongGoi tinhDongGoi = new TinhDongGoi();
+        tinhDongGoi.setName("Auto");
+        System.out.println(tinhDongGoi.getName());
+
+        tinhDongGoi.setName("Auto Test");
+        System.out.println(tinhDongGoi.getName());
+    }
+}
